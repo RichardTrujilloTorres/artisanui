4cf4f19e91e23ad01a527eee8b90e2b9421e0ea8
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-08-20 23:13:13 +0200

    Update config file

--- config/artisanui.php
+++ config/artisanui.php
@@ -2,4 +2,28 @@
 
 return [
     'name' => 'ArtisanUI',
+
+    'api' => [
+        'prefix' => env('ARTISANUI_API_PREFIX', 'api'),
+        'version' => env('ARTISANUI_API_VERSION', 'v1'),
+    ],
+
+    'commands' => [
+        'namespaces' => [
+            'excluded' => [
+
+                'default' => [
+                    'help',
+                    'list',
+                    'packager',
+                    'vendor',
+                    'ui',
+                ],
+
+                'custom' => [
+                    // Include here your custom command namespaces which will be ignored
+                ],
+            ],
+        ],
+    ],
 ];
